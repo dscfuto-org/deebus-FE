0b4ecdc103989fd1a3e6b2ec43ad314c8fce8a9b
tobecci <ojinakatochukwu@gmail.com>
2020-02-27 06:41:56 +0100

    finished the signup and sign in pages

--- src/components/Signin.vue
+++ src/components/Signin.vue
@@ -1,10 +1,55 @@
 <template>
-  <v-container>
-      <v-card min-width="300" min-height="500">
-        <div class="text-center">
-          Signin component
-        </div>
+  <v-container fill-height>
+      <v-row justify="center">
+        <v-card class="pa-10 ml-5" min-width="350" flat>
+        <!-- <v-img src="@/assets/logo-blue.png" width="80"></v-img> -->
+        <v-img src="../assets/logo-blue.png" class="" width="80"></v-img>
+        <div class="display-1 font-weight-bold" id="get-started">Sign in</div>
+        <div class="subtitle">Continue with DeeBus</div>
+        <v-text-field
+        outlined=""
+        placeholder="Email Address"
+        class="mt-4 body-2"
+        color="#6372C1"
+        dense
+        id="full-name"
+        
+        
+        >
+        </v-text-field>
+        <v-text-field
+        outlined=""
+        placeholder="Password"
+        color="#6372C1"
+        class="body-2"
+        dense
+        >
+        </v-text-field>
+        <!-- <v-text-field
+        outlined=""
+        placeholder="Phone Number"
+        color="#6372C1"
+        dense
+        class="body-2"
+        >
+        </v-text-field> -->
+
+        <v-row justify="center">
+          <v-btn rounded="" class="text-capitalize" color="#6372C1">
+          <div
+          class="white--text font-weight-bold mx-5"
+          id="button-text"
+          >
+            Sign in
+          </div>
+        </v-btn>
+      </v-row>
+      <div class="mt-4" id="account-text">
+        New to Deebus?
+        <a href="signup" class="#6372C1--text">Sign up</a>
+      </div>
       </v-card>
+      </v-row>
   </v-container>
 </template>
 
@@ -17,3 +62,23 @@
     }),
   }
 </script>
+
+<style scoped>
+#account-text{
+  font-size:12px;
+  font-weight: bold;
+}
+ 
+#get-started{
+font-family:ubuntu;
+}
+
+#full-name{
+  height: 10px;
+}
+
+#button-text{
+  font-size:13px;
+  font-family: ubuntu;
+}
+</style>
--- src/components/Signup.vue
+++ src/components/Signup.vue
@@ -1,10 +1,55 @@
 <template>
-  <v-container>
-      <v-card min-width="300" min-height="500">
-        <div class="text-center">
-          Signup component
-        </div>
+  <v-container fill-height>
+      <v-row justify="center">
+        <v-card class="pa-10 ml-5" min-width="350" flat>
+        <!-- <v-img src="@/assets/logo-blue.png" width="80"></v-img> -->
+        <v-img src="../assets/logo-blue.png" width="80"></v-img>
+        <div class="display-1 font-weight-bold" id="get-started">Get started</div>
+        <div class="subtitle">Sign up with DeeBus</div>
+        <v-text-field
+        outlined=""
+        placeholder="Full Name"
+        class="mt-4 body-2"
+        color="#6372C1"
+        dense
+        id="full-name"
+        
+        
+        >
+        </v-text-field>
+        <v-text-field
+        outlined=""
+        placeholder="Email Address"
+        color="#6372C1"
+        class="body-2"
+        dense
+        >
+        </v-text-field>
+        <v-text-field
+        outlined=""
+        placeholder="Phone Number"
+        color="#6372C1"
+        dense
+        class="body-2"
+        >
+        </v-text-field>
+
+        <v-row justify="center">
+          <v-btn rounded="" class="text-capitalize" color="#6372C1">
+          <div
+          class="white--text font-weight-bold mx-5"
+          id="button-text"
+          >
+            Continue
+          </div>
+        </v-btn>
+      </v-row>
+      <div class="mt-4" id="account-text">
+        Already have an account?
+        <a href="signin" class="#6372C1--text">Sign in</a>
+      </div>
       </v-card>
+      </v-row>
   </v-container>
 </template>
 
@@ -17,3 +62,23 @@
     }),
   }
 </script>
+
+<style scoped>
+#account-text{
+  font-size:12px;
+  font-weight: bold;
+}
+ 
+#get-started{
+font-family:ubuntu;
+}
+
+#full-name{
+  height: 10px;
+}
+
+#button-text{
+  font-size:13px;
+  font-family: ubuntu;
+}
+</style>
